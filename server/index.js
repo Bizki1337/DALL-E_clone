b13faba5b96f8bbd3789f3be6e872149cce6a8e1
Соловьёв Николай <bizki@MacBook-Pro-Solovev.local>
2023-07-26 20:32:03 +0300

    added mongodb connect

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import connectDB from './mongodb/connect.js'
+
 dotenv.config()
 
 const app = express()
@@ -13,7 +15,12 @@ app.get('/', async (req, res) => {
 })
 
 const startServer = async () => {
-	app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+	try {
+		connectDB(process.env.MONGODB_URL)
+		app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 startServer()
